faf99ed30750536e1e39365f6a8d800b39401e4c
EnriqueMR2026 <enriquemorterareyes@gmail.com>
2026-02-07 04:44:21 -0600

    ajuste menu"

--- development_behavior_packs/RikoCraft/scripts/main.js
+++ development_behavior_packs/RikoCraft/scripts/main.js
@@ -280,7 +280,7 @@ world.beforeEvents.chatSend.subscribe((event) => {
     }
 
     // ---> CASO A: DETECTOR DEL MENÚ PRINCIPAL (@menu) <---
-    if (message.toLowerCase() === `${CONFIG.PREFIJO}menu`) {
+    if (message.toLowerCase() === `${CONFIG.PREFIJO}menu10`) {
         event.cancel = true; // Evita que salga "@menu" en el chat público
         
         // Usamos system.run para poder abrir la ventana (UI)
